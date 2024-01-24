450a75a696c6289ef4550d9a3bcf24983a848dbf
Roel <roelpuntdrost@gmail.com>
2024-01-25 00:05:45 +0100

    Got CommReceiveByte working

--- src/Comm.cpp
+++ src/Comm.cpp
@@ -1,13 +1,12 @@
-#include "Comm.h"
+#include <string.h> 
 
-#ifdef UNITTEST
-#define PRIVATE
-#else
-#define PRIVATE static
-#endif
+#include "Comm.h"
 
 uint8_t DeviceIdGet() {
 	return 4;
+}
+uint8_t StripOffsetGet() {
+	return 16;
 }
 
 typedef enum {
@@ -18,19 +17,6 @@ typedef enum {
 	COMM_RECEIVE_STATE_CRC,
 } comm_receive_state_t;
 
-PRIVATE const uint8_t COMM_RECEIVE_PREAMBLE_BYTE  = 0xcc;
-PRIVATE const uint8_t COMM_RECEIVE_PREAMBLE_COUNT = 2;
-
-PRIVATE uint8_t   comm_receive_crc               = 0;
-PRIVATE uint16_t  comm_receive_skip_before_count = 0;
-PRIVATE uint16_t  comm_receive_skip_after_count  = 0;
-PRIVATE uint16_t  comm_receive_write_count       = 0;
-PRIVATE uint8_t  *comm_receive_write_pos         = 0;
-PRIVATE uint8_t   comm_receive_address           = 0;
-
-PRIVATE comm_receive_state_t comm_receive_state            = COMM_RECEIVE_STATE_SYNC;
-PRIVATE uint8_t              comm_receive_preamble_counter = 0;
-
 typedef struct  {
 	uint8_t (*get_addr)(void);
 	uint8_t addr_count;
@@ -39,11 +25,8 @@ typedef struct  {
 typedef enum {
 	COMMAND_TYPE_BROADCAST,
 	COMMAND_TYPE_DEVICE,
+	COMMAND_TYPE_STRIP
 } command_type_t;
-
-command_type_info_t cmd_type_infos[] = {
-	{DeviceIdGet, 1}
-};
 
 typedef enum {
 	COMMAND_STATE_UNLOCKED,
@@ -58,32 +41,93 @@ typedef const struct {
 	uint8_t * const      state;
 } comm_receive_command_t;
 
+PRIVATE const uint8_t COMM_RECEIVE_PREAMBLE_BYTE  = 0xcc;
+PRIVATE const uint8_t COMM_RECEIVE_PREAMBLE_COUNT = 2;
+
+PRIVATE uint8_t   comm_receive_crc               = 0;
+PRIVATE uint16_t  comm_receive_skip_before_count = 0;
+PRIVATE uint16_t  comm_receive_skip_after_count  = 0;
+PRIVATE uint16_t  comm_receive_write_count       = 0;
+PRIVATE uint8_t  *comm_receive_write_pos         = 0;
+PRIVATE uint8_t   comm_receive_address           = 0;
+
+PRIVATE uint8_t   comm_receive_error_state       = 0;
+
+PRIVATE comm_receive_state_t comm_receive_state            = COMM_RECEIVE_STATE_SYNC;
+PRIVATE uint8_t              comm_receive_preamble_counter = 0;
+
+PRIVATE const command_type_info_t cmd_type_infos[] = {
+	{DeviceIdGet, 1},
+	{StripOffsetGet, 4},
+};
+
 PRIVATE comm_receive_command_t *comm_receive_command = 0;
 
-uint8_t command1_state = COMMAND_STATE_UNLOCKED;
-uint8_t command1_buffer[4];
-void (*command1_on_recv_func)(uint8_t buffer[4]) = 0;
-comm_receive_command_t command1 = {
+PRIVATE uint8_t command1_state = COMMAND_STATE_UNLOCKED;
+PRIVATE uint8_t command1_buffer[4];
+PRIVATE void (*command1_on_recv_func)(uint8_t buffer[4]) = 0;
+PRIVATE const comm_receive_command_t command1 = {
 	COMMAND_TYPE_BROADCAST,
 	command1_buffer,
 	sizeof(command1_buffer),
 	&command1_state
 };
 
-uint8_t command2_state = COMMAND_STATE_UNLOCKED;
-uint8_t command2_buffer[4];
-void (*command2_on_recv_func)(uint8_t buffer[4]) = 0;
-comm_receive_command_t command2 = {
+PRIVATE uint8_t command2_state = COMMAND_STATE_UNLOCKED;
+PRIVATE uint8_t command2_buffer[4];
+PRIVATE void (*command2_on_recv_func)(uint8_t buffer[4]) = 0;
+PRIVATE const comm_receive_command_t command2 = {
 	COMMAND_TYPE_DEVICE,
 	command2_buffer,
 	sizeof(command2_buffer),
 	&command2_state
 };
 
-PRIVATE comm_receive_command_t* comm_receive_commands[] = {	
-	&command1,
-	&command2
+PRIVATE uint8_t command3_state = COMMAND_STATE_UNLOCKED;
+PRIVATE uint8_t command3_buffer[2*4];
+PRIVATE void (*command3_on_recv_func)(uint8_t buffer[1]) = 0;
+PRIVATE const comm_receive_command_t command3 = {
+	COMMAND_TYPE_STRIP,
+	command3_buffer,
+	2,
+	&command3_state
 };
+
+PRIVATE const comm_receive_command_t* comm_receive_commands[] = {	
+	&command1,
+	&command2,
+	&command3,
+};
+
+#ifdef UNITTEST
+void CommReceiveUnittestReset() {
+	comm_receive_crc               = 0;
+	comm_receive_skip_before_count = 0;
+	comm_receive_skip_after_count  = 0;
+	comm_receive_write_count       = 0;
+	comm_receive_write_pos         = 0;
+	comm_receive_address           = 0;
+	
+	comm_receive_error_state       = 0;
+
+	comm_receive_state            = COMM_RECEIVE_STATE_SYNC;
+	comm_receive_preamble_counter = 0;
+
+	comm_receive_command = 0;
+
+	command1_state = COMMAND_STATE_UNLOCKED;
+	memset(command1_buffer, 0, 4);
+	command1_on_recv_func = 0;
+
+	command2_state = COMMAND_STATE_UNLOCKED;
+	memset(command2_buffer, 0, 4);
+	command2_on_recv_func = 0;
+
+	command3_state = COMMAND_STATE_UNLOCKED;
+	memset(command3_buffer, 0, 2*4);
+	command3_on_recv_func = 0;
+}
+#endif
 
 void CommReceiveByte(uint8_t b) {
 	
@@ -109,6 +153,9 @@ void CommReceiveByte(uint8_t b) {
 		
 		if(b != COMM_RECEIVE_PREAMBLE_BYTE) {
 			comm_receive_preamble_counter = 0;
+			if(!comm_receive_error_state) {
+				comm_receive_error_state = COMM_RECEIVE_ERROR_STATE_UNSYNC;
+			}
 			return;
 		}
 		if (++comm_receive_preamble_counter >= COMM_RECEIVE_PREAMBLE_COUNT) {
@@ -118,8 +165,16 @@ void CommReceiveByte(uint8_t b) {
 		}
 		return;
 	case COMM_RECEIVE_STATE_COMMAND_ID:
-		// TODO bound check
-		comm_receive_command = comm_receive_commands[b];
+	{
+		uint8_t cmd_id = b;
+		if (cmd_id >= sizeof(comm_receive_commands)/sizeof(comm_receive_commands[0])) {
+			if(!comm_receive_error_state) {
+				comm_receive_error_state = COMM_RECEIVE_ERROR_STATE_COMMAND_ID;
+			}
+			comm_receive_state = COMM_RECEIVE_STATE_SYNC;
+			return;
+		}
+		comm_receive_command = comm_receive_commands[cmd_id];
 		
 		if (comm_receive_command->type == COMMAND_TYPE_BROADCAST) {
 			if (*(comm_receive_command->state) != COMMAND_STATE_UNLOCKED) {
@@ -136,6 +191,7 @@ void CommReceiveByte(uint8_t b) {
 		}
 		comm_receive_state = COMM_RECEIVE_STATE_ADDRESS;
 		return;
+	}
 	case COMM_RECEIVE_STATE_ADDRESS:
 		comm_receive_address = b;
 		comm_receive_state   = COMM_RECEIVE_STATE_COUNT;
@@ -144,7 +200,7 @@ void CommReceiveByte(uint8_t b) {
 	{
 		uint8_t count = b;
 		
-		command_type_info_t& cmd_type_info = cmd_type_infos[comm_receive_command->type - 1];
+		const command_type_info_t& cmd_type_info = cmd_type_infos[comm_receive_command->type - 1];
 		uint8_t my_addr       = cmd_type_info.get_addr();
 		uint8_t my_addr_count = cmd_type_info.addr_count;
 		uint8_t block_size    = comm_receive_command->receive_buffer_size;
@@ -154,8 +210,6 @@ void CommReceiveByte(uint8_t b) {
 			comm_receive_state = COMM_RECEIVE_STATE_CRC;
 			return;
 		}
-		*(comm_receive_command->state) = COMMAND_STATE_WRITE_LOCKED;
-		
 		
 		uint8_t skip_before_read;
 		uint8_t read;
@@ -169,15 +223,26 @@ void CommReceiveByte(uint8_t b) {
 				read = count - skip_before_read;
 				if (read > my_addr_count) {
 					skip_after_read = read - my_addr_count;
-					read = my_addr_count;
 					comm_receive_skip_after_count = skip_after_read * block_size;
+					read = my_addr_count;
 				}
 				comm_receive_write_count = read * block_size;
 				comm_receive_write_pos   = comm_receive_command->receive_buffer;
 			}
 			comm_receive_skip_before_count = skip_before_read * block_size;
 		} else {
-			*((int*)0)=0;
+			uint8_t my_addr_end = my_addr + my_addr_count;
+			read = (comm_receive_address < my_addr_end)
+					? my_addr_end - comm_receive_address
+					: 0;
+			comm_receive_write_count = read * block_size;
+			comm_receive_write_pos   = comm_receive_command->receive_buffer + (comm_receive_address - my_addr) * block_size;
+			
+			skip_after_read = count - read;
+			comm_receive_skip_after_count = skip_after_read * block_size;
+		}
+		if(comm_receive_write_count) {
+			*(comm_receive_command->state) = COMMAND_STATE_WRITE_LOCKED;
 		}
 		comm_receive_state = COMM_RECEIVE_STATE_CRC;
 		return;
@@ -185,6 +250,9 @@ void CommReceiveByte(uint8_t b) {
 	case COMM_RECEIVE_STATE_CRC:
 		if (comm_receive_crc != 0) {
 			*(comm_receive_command->state) = COMMAND_STATE_UNLOCKED;
+			if(!comm_receive_error_state) {
+				comm_receive_error_state = COMM_RECEIVE_ERROR_STATE_CRC;
+			}
 			comm_receive_state = COMM_RECEIVE_STATE_SYNC;
 			return;
 		}
--- src/Comm.h
+++ src/Comm.h
@@ -3,7 +3,24 @@
 
 #include "stdint.h"
 
+#ifdef UNITTEST
+#define PRIVATE
+#else
+#define PRIVATE static
+#endif
+
 #define COMMMAND_TEST 0
+
+typedef enum {
+	COMM_RECEIVE_ERROR_STATE_NONE,
+	COMM_RECEIVE_ERROR_STATE_UNSYNC,
+	COMM_RECEIVE_ERROR_STATE_COMMAND_ID,
+	COMM_RECEIVE_ERROR_STATE_CRC
+} comm_receive_error_state_t;
+
+#ifdef UNITTEST
+void CommReceiveUnittestReset();
+#endif
 
 void CommReceiveLoop();
 
